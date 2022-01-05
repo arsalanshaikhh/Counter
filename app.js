802a28cc4b11b9845d85daa6b530fe455f7de521
Arsalan Shaikh <88669777+coder-arsalan@users.noreply.github.com>
2022-01-05 22:24:47 +0530

    counter

---
+++ app.js
@@ -0,0 +1,23 @@
+var count = 0;
+document.querySelector("#clr").addEventListener("click", clear);
+function clear() {
+  count = 0;
+  document.querySelector("h1").textContent = count;
+}
+
+document.querySelector("#inc").addEventListener("click", increment);
+
+function increment() {
+  count = count + 1;
+  document.querySelector("h1").textContent = count;
+}
+
+document.querySelector("#dec").addEventListener("click", decrement);
+function decrement() {
+  if (count <= 0) {
+    document.querySelector("#dec").removeEventListener("click", decrement);
+  } else {
+    count = count - 1;
+  }
+  document.querySelector("h1").textContent = count;
+}
